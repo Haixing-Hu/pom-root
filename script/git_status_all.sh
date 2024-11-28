65b9b65a762a5c8eeb4628aa7529a45569625f98
Haixing Hu <starfish.hu@gmail.com>
2024-11-29 02:50:00 +0800

    build: update distributionManagement

--- script/git_status_all.sh
+++ script/git_status_all.sh
@@ -2,7 +2,7 @@
 
 ################################################################################
 #
-#    Copyright (c) 2022 - 2023.
+#    Copyright (c) 2022 - 2024.
 #    Haixing Hu, Qubit Co. Ltd.
 #
 #    All rights reserved.
